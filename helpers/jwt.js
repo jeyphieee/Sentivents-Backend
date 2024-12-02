f242db9dd50afb02e873678eb31d56d09ac8b1e0
John Paul C. Francisco <johnpaul.francisco@tup.edu.ph>
2024-12-02 15:14:32 +0800

    try1

--- helpers/jwt.js
+++ helpers/jwt.js
@@ -28,6 +28,9 @@ function authJwt() {
                     methods: ['GET', 'POST', 'PUT', 'DELETE','OPTIONS']
                 },
                 { url: /\/public\/uploads(.*)/, methods: ['GET']},
+                { url: /\/api\/v1\/\/public\/uploads(.*)/, methods: ['GET']},
+                { url: /\/sentivents-backend.onrender.com\/api\/v1\/\/public\/uploads(.*)/, methods: ['GET']},
+
                 `${api}/users`,
                 `${api}/users/login`,
                 `${api}/users/email/:email`,
